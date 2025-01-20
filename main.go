193282d7182bd22042f37ab64333480673810fcc
jeremyholbrecht <griffley@proton.me>
2025-01-20 10:37:15 +0100

    changed timer to cleaner if statement

--- main.go
+++ main.go
@@ -9,29 +9,16 @@ import (
 
 func main() {
 
-	timer := flag.Float64("t", 5, "Set the countdown timer")
+	set_timer := flag.Float64("t", 5, "Set the countdown timer")
 
 	flag.Parse()
 
-	switch *timer {
-	case 1:
-		fmt.Println("1sec ticking....")
-	case 2:
-		fmt.Println("2secs ticking....")
-		timer := time.NewTimer(2 * time.Second)
-		<-timer.C
-		fmt.Println("Time is up!")
-	case 3:
-		fmt.Println("3secs ticking....")
-		timer := time.NewTimer(3 * time.Second)
+	if *set_timer != 0 {
+		fmt.Printf("%v secs ticking... \n", *set_timer)
+		timer := time.NewTimer(time.Duration(*set_timer * float64(time.Second)))
 		<-timer.C
 		fmt.Println("Time is up!")
 		log.Fatal(play_alarm())
-
-	case 4:
-		fmt.Println("4sec ticking....")
-	case 5:
-		fmt.Println("5sec ticking....")
 
 	}
 
