60afd7767914032a7df8d4d848d79c74d9c7f1c0
jeremyholbrecht <griffley@proton.me>
2025-01-20 09:41:06 +0100

    moved alarm function to it's own file

--- main.go
+++ main.go
@@ -4,11 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"os"
 	"time"
-
-	"github.com/hajimehoshi/go-mp3"
-	"github.com/hajimehoshi/oto/v2"
 )
 
 func main() {
@@ -39,40 +35,4 @@ func main() {
 
 	}
 
-}
-
-func play_alarm() error {
-	// Open the file
-	file, err := os.Open("alarm.mp3")
-	if err != nil {
-		return err
-	}
-
-	defer file.Close()
-
-	//decode the file
-	decoder, err := mp3.NewDecoder(file)
-	if err != nil {
-		return err
-	}
-
-	c, ready, err := oto.NewContext(decoder.SampleRate(), 2, 2)
-	if err != nil {
-		return err
-	}
-	<-ready
-
-	//create a new player that will handle the sound, paused by default
-	player := c.NewPlayer(decoder)
-	defer player.Close()
-	player.Play()
-
-	for {
-		time.Sleep(time.Second)
-		if !player.IsPlaying() {
-			break
-		}
-	}
-
-	return nil
 }
